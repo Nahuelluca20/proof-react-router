23f72985f41754868c029938de65d96a7e781754
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-25 19:29:58 -0300

    chore: Update dependencies and Vite configuration
    This commit updates the dependencies in the `package.json` file, adding `@cloudflare/workers-types` package. It also modifies the `vite.config.ts` file, reordering the plugins array to improve the Vite configuration.
    
    The addition of `@cloudflare/workers-types` package ensures that the necessary types are available for Cloudflare Workers integration. The reordering of plugins in `vite.config.ts` ensures that the `cloudflareDevProxyVitePlugin` is applied before other plugins, improving the development experience.
    
    This commit is a chore because it includes updates and modifications that do not directly affect the functionality of the application.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,17 @@
-import type { MetaFunction } from "react-router";
+import { json, useLoaderData, type MetaFunction } from "react-router";
 
 export const meta: MetaFunction = () => {
   return [{ title: "New React Router App" }];
 };
 
+export const loader = () => {
+  console.log("object");
+  return json("gola");
+};
+
 export default function Index() {
+  const data = useLoaderData();
+  console.log(data);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to React Router 😉 (with Vite and Cloudflare)</h1>
@@ -20,7 +27,7 @@ export default function Index() {
         </li>
         <li>
           <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
+            {data}
           </a>
         </li>
       </ul>
